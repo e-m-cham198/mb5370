d354b6439e179140a2d76905bc9e95b1ff94c89c
e-m-cham198 <emilee.chamberland@my.jcu.edu.au>
2023-05-14 14:58:46 +1000

    Plot deconstruction script updated, cleaned and styled for later reference and reproducibility

--- R/PlotDeconstruction.R
+++ R/PlotDeconstruction.R
@@ -1,40 +1,43 @@
-#Workshop 3:Reproducible Work
+#----------------------------------------#
+# MB5371: Module 4 Assessment 1 - Plot Reconstruction
+# Emilee Chamberland
+# May 2023
 
-##4.1 Setting up Git and Github
+## Workspace: "C:\Users\custo\OneDrive\Documents\JCU _2023\SP1\MB5370\Module4_R\github\mb5370\R\PlotDeconstruction.R"
+#------------------------------------------#
+#Load tidyverse package
 
-#Github allows version control of your work and shareability of your code, notes, and figures. 
+library(tidyverse)
+#------------------------------------------#
 
-#install.packages("usethis") #after setting up Github account this package is used to connect R Studio to your github repository
-library(usethis)
-use_git_config(user.name = "e-m-cham198", user.email = "emilee.chamberland@my.jcu.edu.au")
+# Original graph data 
 
+#------------------------------------------#
 
+# Original graph acquired from https://horserookie.com/cost-of-owning-a-horse-2020-expense-report-summary/
+# Data extracted from the graph using https://automeris.io/WebPlotDigitizer/ and saved as a .csv file
 
+#------------------------------------------#
 
-hc <- read.csv("C:/Users/custo/Dropbox/PC/Downloads/Value (1).csv") #bad graph data
+# Load the data
 
-#first run at reconstruction
-ggplot(data = hc, mapping = aes(x = Month, y = Cost, group = Barter, color = Barter)) +
- # geom_line() +
-  geom_point() +
-  labs(subtitle = "Value vs After Barters",
-     caption = "Data from horserookie.com") 
-  scale_x_continuous("month", breaks = seq(1,12, by = 1), limits = c(1,12))#, labels = c("jan", "feb", "Mar", "Apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"))
+hc <- read.csv("~/JCU _2023/SP1/MB5370/Module4_R/github/mb5370/data/Plot_Recon_Data.csv") #.csv file containing the extracted data from the 'bad graph'
+# Data can be located in Github repository under "data"
 
+#------------------------------------------#
 
+# Plot Reconstruction
 
-test <- read.csv("C:/Users/custo/Dropbox/PC/Downloads/test.csv") #est data
-
-ggplot(data = hc, mapping = aes(x = Month, y = Cost)) +
-  geom_point(mapping = aes(group = Barter, color = Barter)) +
-  geom_line(mapping = aes(group = Barter, color = Barter)) +
-  scale_x_continuous("Month", breaks = seq(1,12, by = 1), labels = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")) +
-  scale_y_continuous("Dollars ($)", breaks = seq(500,3000, by = 500), limits = c(500, 3000)) +
-  scale_color_manual(values = c("Value" = "darkgreen", "After Barters" = "springgreen")) +
-  theme_classic() +
-  labs(title = "Cost of horse ownership in 2020", 
-       subtitle = "Monthly value of expenses vs Barter payouts",
-       caption = "Data from horserookie.com") +
-  theme(legend.title = element_blank())
+ggplot(data = hc, mapping = aes(x = Month, y = Cost)) + # set the coordinate space and data
+  geom_point(mapping = aes(group = Barter, color = Barter)) + #add point data layer
+  geom_line(mapping = aes(group = Barter, color = Barter)) + #add line data layer to connect the points on the graph
+  scale_x_continuous("Month", breaks = seq(1,12, by = 1), labels = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")) + #set the x axis label, breaks and tick labels
+  scale_y_continuous("Dollars ($)", breaks = seq(500,3000, by = 500), limits = c(500, 3000)) + #set the y axis label, breaks and limits
+  scale_color_manual(values = c("Value" = "darkgreen", "After Barters" = "springgreen")) + #set the color scales for the data
+  theme_classic() + #set the theme - classic theme has x & y axis lines with no gridlines
+  labs(title = "Cost of horse ownership in 2020", #set the title
+       subtitle = "Monthly value of expenses vs Barter payouts", #set the subtitle, sits below the title to add additional information
+       caption = "Data from horserookie.com") + # set the caption - where the data was acquired or any other additional information
+  theme(legend.title = element_blank()) #remove the legend title
   
 
